f19ca167edac6204451c43e3de85783ef4c21197
Seungtae Lee <90381057+SingToLive@users.noreply.github.com>
2022-06-30 18:03:09 +0900

    MOD urls.py
    result page 경로 설정

--- third_project_frontend/urls.py
+++ third_project_frontend/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from third_project_frontend import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('result/', views.result_view, name='result'),
 ]
