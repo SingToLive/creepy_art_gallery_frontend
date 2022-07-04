797c87da7ed7f26a751418986f816747b0dc9b57
Seungtae Lee <90381057+SingToLive@users.noreply.github.com>
2022-07-04 14:10:47 +0900

    MOD views.py
    주석

--- third_project_frontend/views.py
+++ third_project_frontend/views.py
@@ -1,13 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def result_view(request):
-    return render(request, "result.html")
-
+    # loginUser = request.user
+    # if not loginUser.is_authenticated:
+    #     return redirect('/sign_in')
+    return render(request, 'result.html')
 
 def sign_in_view(request):
-    return render(request, "user/signin.html")
-
+    print(request.user)
+    loginUser = request.user
+    if loginUser.is_authenticated:
+        return redirect('/')
+    return render(request, 'user/signin.html')
 
 def sign_up_view(request):
     return render(request, "user/signup.html")
